50fa38d86789f8897c53dc7362a4acc2ca29e1ba
Carolina <CARITOBC1904@GMAIL.COM>
2026-05-26 23:45:16 -0500

    actualizacion completa polla mundialista

--- app.py
+++ app.py
@@ -5,9 +5,25 @@ st.set_page_config(
     page_icon='⚽',
     layout='wide'
 )
+st.markdown('''
+<style>
 
-st.title('⚽ POLLA MUNDIALISTA EMPRESARIAL')
+.stApp {
+    background-color: #0b0f1a;
+    color: white;
+}
+
+h1 {
+    color: gold;
+    text-align: center;
+}
+
+</style>
+''', unsafe_allow_html=True)
+st.title('⚽ POLLA MUNDIALISTA 2026 HACIENDA LA CABAÑA 🌴')
 
 st.markdown('## Bienvenidos')
 
 st.write('Selecciona una opción en el menú lateral.')
+
+st.image('assets/C:\\Users\\HP\\Documents\\Documentos\\LA CABAÑA\\COPASST\\LOGOS E IMAGENS\\WhatsApp Image 2024-12-10 at 7.50.54 AM.jpeg', width=250)
